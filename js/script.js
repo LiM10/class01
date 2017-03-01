d0a385ed43aebc6ffee5c3cda4df09e0521e69b6
Emrah Ismayilzade <Emrah Ismayilzade>
2017-03-01 18:04:45 +0400

    01.03.2017

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+
+function futbolcular(ad,nomre,klub,olke){
+		this.ad=ad;
+		this.nomre=nomre;
+		this.klub=klub;
+		this.olke=olke;
+}
+
+var rani = new futbolcular("Ronaldinho","10","Barcelona","Braziliya");
+var lion = new futbolcular("Puyol","5","Barcelona","Ispaniya");
+var maestro = new futbolcular("Xavi","6","Barcelona","Ispaniya");
+var casper = new futbolcular("Iniesta","8","Barcelona","Ispaniya");
+
+console.log(rani);
+console.log(lion);
+console.log(maestro);
+console.log(casper);
